0d3695147826aca263652742d1fe8bf609ef5829
Malcon Augusto Souza de Miranda <malcon.miranda@nm.local>
2022-08-10 18:07:22 -0300

    feat: passando flags como argumento

--- iniciando-pratica/process.js
+++ iniciando-pratica/process.js
@@ -1,4 +1,5 @@
 
-const firstName = process.argv[2]
-const lastName = process.argv[3]
-console.log(`Seu nome é ${firstName} ${lastName}`);
+// const firstName = process.argv[2]
+// const lastName = process.argv[3]
+// console.log(`Seu nome é ${firstName} ${lastName}`);
+console.log(process.argv)

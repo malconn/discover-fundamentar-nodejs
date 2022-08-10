4528f2dd185738ccc3c6e56f495a14bf3d17c8bb
Malcon Augusto Souza de Miranda <malcon.miranda@nm.local>
2022-08-10 17:54:45 -0300

    feat: criando modulos

--- iniciando-pratica/require.js
+++ iniciando-pratica/require.js
@@ -1,2 +1,9 @@
-const path = require('path')
-console.log(path.basename(__filename))
+// módulos nativos
+
+// const path = require('path')
+// console.log(path.basename(__filename))
+
+
+//meus módulos
+const myModule = require('./exports.js');
+console.log(myModule);

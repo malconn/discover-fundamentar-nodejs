c7602c53abaeb22d6267d51b34f0ca0bbeba924a
Malcon Augusto Souza de Miranda <malcon.miranda@nm.local>
2022-08-16 16:29:47 -0300

    feat: finalizando trilha node

--- events.js
+++ events.js
@@ -1,7 +1,16 @@
+const { inherits} = require('util')
 const { EventEmitter} = require('events')
 
-const ev = new EventEmitter
-ev.once('saySomething',(message)=>{
- console.log('Eu ouvi vc:', message)
-})
-ev.emit('saySomething',"Malcon")
+function Character(name){
+ this.name = name
+}
+
+inherits(Character, EventEmitter)
+
+const chapolin = new Character('Chapolin');
+
+chapolin.on('help',()=> console.log(`Eu! o ${chapolin.name} colorado!`))
+
+console.log('Chamar chapolin');
+
+chapolin.emit('help');
